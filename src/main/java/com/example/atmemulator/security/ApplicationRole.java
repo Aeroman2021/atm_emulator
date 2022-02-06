4be3497b0fe8c6d82d60d9357a6fb22c5cd7945d
Mohsen <malakouti.aero@gmail.com>
2022-02-06 18:33:40 +0330

    creditcard as the main role added.

---
+++ src/main/java/com/example/atmemulator/security/ApplicationRole.java
@@ -0,0 +1,32 @@
+package com.example.atmemulator.security;
+
+import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum ApplicationRole {
+    CREDIT_CARD(Sets.newHashSet(ApplicationPermission.values()));
+
+    private final Set<ApplicationPermission> permissions;
+
+    ApplicationRole(Set<ApplicationPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthority(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+
+    }
+}
