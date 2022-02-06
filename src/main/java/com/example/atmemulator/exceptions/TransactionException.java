0340abd5151f0bb4914244fe5c5149f40a1c660c
Mohsen <malakouti.aero@gmail.com>
2022-02-06 18:20:27 +0330

    transaction exception added

---
+++ src/main/java/com/example/atmemulator/exceptions/TransactionException.java
@@ -0,0 +1,14 @@
+package com.example.atmemulator.exceptions;
+
+public class TransactionException extends RuntimeException{
+    public TransactionException() {
+    }
+
+    public TransactionException(String message) {
+        super(message);
+    }
+
+    public TransactionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
