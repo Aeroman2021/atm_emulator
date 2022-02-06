c04b5988dde417eb619b6f3abd05c28d406631c1
Mohsen <malakouti.aero@gmail.com>
2022-02-06 18:20:54 +0330

    NotFoundException added.

---
+++ src/main/java/com/example/atmemulator/exceptions/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.atmemulator.exceptions;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
