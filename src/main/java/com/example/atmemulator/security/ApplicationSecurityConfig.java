cb952af3878adee78bd9d4bc131193fec86b39e4
Mohsen <malakouti.aero@gmail.com>
2022-02-06 18:33:08 +0330

    configure method added for the authentication of the creditcard  added.

---
+++ src/main/java/com/example/atmemulator/security/ApplicationSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.example.atmemulator.security;
+
+import com.example.atmemulator.jwt.JwtConfig;
+import com.example.atmemulator.jwt.JwtPinCodeAndCardNumberAuthenticationFilter;
+import com.example.atmemulator.jwt.JwtTokenVerifier;
+import com.example.atmemulator.security.creditcard.CreditCardManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
+    private final PasswordEncoder passwordEncoder;
+    private final CreditCardManager creditCardManager;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilter(new JwtPinCodeAndCardNumberAuthenticationFilter(authenticationManager(),jwtConfig,secretKey))
+                .addFilterAfter(new JwtTokenVerifier(jwtConfig,secretKey),JwtPinCodeAndCardNumberAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider (){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(creditCardManager);
+        return provider;
+    }
+
+
+
+
+}
